614707e883bec0b7a3734a2ddd1297840c34c1b1
lieto <myke.lieto@etu.univ-amu.fr>
2025-02-04 12:04:54 +0100

    Initial Commit

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Dynamicqcm;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // Tableau de thèmes
+        $themes = [
+            'Science-fiction',
+            'Fantasy',
+            'Horreur',
+            'Romance',
+            'Aventure',
+            'Mystery',
+            'Thriller',
+            'Comédie',
+            'Drame',
+            'Histoire'
+        ];
+
+        // Boucle pour ajouter chaque thème dans la base de données
+        foreach ($themes as $theme) {
+            $dynamicQcm = new Dynamicqcm();
+            $dynamicQcm->setTheme($theme); // Assurez-vous d'avoir un setter pour 'theme'
+
+            $manager->persist($dynamicQcm);
+        }
+
+        // Enregistrer dans la base de données
+        $manager->flush();
+    }
+}
